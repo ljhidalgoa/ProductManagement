4313a2c6842e9bc5b2c4f1ceedffc249b3ec2397
INDRA\ljhidalgo <ljhidalgo@indracompany.com>
2023-01-19 02:21:22 -0500

    Se crea el enumerador del Rating y se mejora la clase Product

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -18,23 +18,25 @@ package labs.pm.data;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import static labs.pm.data.Rating.*;
 
 /**
- * {@code Product} class represents properties and behaviours of 
- * products objects in the Product Management System
+ * {@code Product} class represents properties and behaviours of products
+ * objects in the Product Management System
  * <br>
  * Each product has an id, name and price
  * <br>
  * Each product can have a discount, calculated based on a
  * {@link DISCOUNT_RATE discount rate}
+ *
  * @version 4.0
  * @author ljhidalgo
  */
 public class Product {
 
     /**
-     * A constant that defines a
-     * {@link java.math.BigDecimal BigDecimal} value of the discount rate
+     * A constant that defines a {@link java.math.BigDecimal BigDecimal} value
+     * of the discount rate
      * <br>
      * Discount rate is 10%
      */
@@ -42,41 +44,62 @@ public class Product {
     private int id;
     private String name;
     private BigDecimal price;
+    private Rating rating;
+
+    public Product() {
+        this(0, "no-name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
-
+//    public void setId(final int id) {
+//        this.id = id;
+//    }
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
+//    public void setName(final String name) {
+//        this.name = name;
+//    }
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-        // price = BigDecimal.ONE;
-        this.price = price;
-    }
-    
+//    public void setPrice(final BigDecimal price) {
+////        price = BigDecimal.ONE;
+//        this.price = price;
+//    }
     /**
      * Claculates discount based on a product price and
      * {@link DISCOUNT_RATE discount rate}
-     * @return a {@link java.math.BigDecimal BigDecimal}
-     * value of the discount
+     *
+     * @return a {@link java.math.BigDecimal BigDecimal} value of the discount
      */
     public BigDecimal getDiscount() {
         return price.multiply(DISCOUNT_RATE)
                 .setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public Product applyRating(Rating newRating) {
+        return new Product(this.id, this.name, this.price, newRating);
     }
 
 }
