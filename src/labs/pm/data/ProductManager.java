555211c752d6aa8c0efbace7cc00d55dad047fce
INDRA\ljhidalgo <ljhidalgo@indracompany.com>
2023-01-22 03:07:01 -0500

    Implement review sort and product search features

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -61,6 +62,10 @@ public class ProductManager {
         products.putIfAbsent(product, new ArrayList<>());
         return product;
     }
+    
+    public Product reviewProduct(int id, Rating rating, String comments) {
+        return reviewProduct(findProduct(id), rating, comments);
+    }
 
     public Product reviewProduct(Product product, Rating rating, String comments) {
         List<Review> reviews = products.get(product);
@@ -74,6 +79,21 @@ public class ProductManager {
         products.put(product, reviews);
         return product;
     }
+    
+    public Product findProduct(int id) {
+        Product result = null;
+        for(Product product : products.keySet()) {
+            if(product.getId() == id) {
+                result = product;
+                break;
+            }
+        }
+        return result;
+    }
+    
+    public void printProductReport(int id) {
+        printProductReport(findProduct(id));
+    }
 
     public void printProductReport(Product product) {
         List<Review> reviews = products.get(product);
@@ -84,6 +104,7 @@ public class ProductManager {
                 product.getRating().getStars(),
                 dateFormat.format(product.getBestBefore())));
         txt.append("\n");
+        Collections.sort(reviews);
         for(Review review : reviews) {
             txt.append(MessageFormat.format(resources.getString("review"),
                     review.getRating().getStars(),
